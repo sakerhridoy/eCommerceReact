e8d431bd629e049d1d702402cfacf724434e5b0c
hridoy <sakerahmedhridoy@gmail.com>
2026-02-10 14:57:29 +0600

    responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+
 import { ShopProvider } from './context/ShopContext.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import CountDownContextProvider from './context/CountDownContext/CountDownContextProvider';
